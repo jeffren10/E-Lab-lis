2ac819c89bbbf6b33d6e03cb48d89854b49ae759
jeffren <ijeffren1@gmail.com>
2024-10-12 19:43:56 +0100

    done work

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const sideMenu = document.querySelector("aside-sidebar");
+const menuBtn = document.querySelector("menu-btn");
+
+menuBtn.addEventListener('click', () =>{
+  sideMenu.style.display = 'block';
+})
+
+
